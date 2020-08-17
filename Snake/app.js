8afd31de2cc3ebc75683394caf3265e70f2a795b
mark <markpackham1@gmail.com>
2020-08-17 15:35:50 +0100

    Initial setup for snake game learned from https://youtu.be/rui2tRRVtc0?t=314

---
+++ Snake/app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const squares = document.querySelectorAll(".grid div");
+  const scoreDisplay = document.querySelector("span");
+  const startBtn = document.querySelector(".start");
+
+  const width = 10;
+  let currentIndex = 0;
+  let appleIndex = 0;
+  let currentSnake = [2, 1, 0];
+  let direction = 1;
+  let score = 0;
+  let speed = 0.9;
+  let intervalTime = 0;
+  let interval = 0;
+});
