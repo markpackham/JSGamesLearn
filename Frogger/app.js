815323b9b092b3f5b6c0b045392093cf5998cc93
mark <markpackham1@gmail.com>
2020-08-18 22:25:48 +0100

    add structure for frogger game, https://youtu.be/L-FWBhjt6CU?t=331

---
+++ Frogger/app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const squares = document.querySelectorAll(".grid div");
+  const logsLeft = document.querySelectorAll(".log-left");
+  const logsRight = document.querySelectorAll(".log-right");
+  const carsLeft = document.querySelectorAll(".car-left");
+  const carsRight = document.querySelectorAll(".car-right");
+  const timeLeft = document.querySelector("#time-left");
+  const result = document.querySelector("#result");
+  const startBtn = document.querySelector("#button");
+  const width = 9;
+  let currentIndex = 76;
+  let currentTime = 20;
+  let timerId;
+});
