ced12bc1255424bf6b45af3cf141cfa709d357ce
mark <markpackham1@gmail.com>
2020-08-20 11:31:23 +0100

    finish off pac-man game

--- Pac-Man/app.js
+++ Pac-Man/app.js
@@ -976,5 +976,31 @@ document.addEventListener("DOMContentLoaded", () => {
     }, ghost.speed);
   }
 
+  // check for a game over
+  function checkForGameOver() {
+    // ghost has to hit pac-man when they aren't a scared ghost
+    if (
+      squares[pacmanCurrentIndex].classList.contains("ghost") &&
+      !squares[pacmanCurrentIndex].classList.contains("scared-ghost")
+    ) {
+      ghosts.forEach((ghost) => clearInterval(ghost.timerId));
+      document.removeEventListener("keyup", movePacman);
+      setTimeout(function () {
+        alert("Game Over");
+      }, 500);
+    }
+  }
+
+  // check for a win - more is when this score is reached
+  function checkForWin() {
+    if (score === 274) {
+      ghosts.forEach((ghost) => clearInterval(ghost.timerId));
+      document.removeEventListener("keyup", movePacman);
+      setTimeout(function () {
+        alert("You have WON!");
+      }, 500);
+    }
+  }
+
   document.addEventListener("keyup", movePacman);
 });
