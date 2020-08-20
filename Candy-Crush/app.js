9c463d0338bab9e8eed97f381627c93cbd32a9a4
mark <markpackham1@gmail.com>
2020-08-20 14:03:48 +0100

    finish off square swapping, https://youtu.be/XD5sZWxwJUk?t=1209

--- Candy-Crush/app.js
+++ Candy-Crush/app.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const squares = [];
   let score = 0;
 
+  // colors red, yellow, orange, purple, green and blue
   const candyColors = [
     "url(images/red-candy.png)",
     "url(images/yellow-candy.png)",
@@ -19,6 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement("div");
       square.setAttribute("draggable", true);
+      // all squares have ids
       square.setAttribute("id", i);
       let randomColor = Math.floor(Math.random() * candyColors.length);
       square.style.backgroundImage = candyColors[randomColor];
@@ -27,4 +29,61 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   createBoard();
+
+  // drag candy
+  let colorBeingDragged;
+  let colorBeingReplaced;
+  let squareIdBeingDragged;
+  let squareIdBeingReplaced;
+
+  squares.forEach((square) => square.addEventListener("dragstart", dragStart));
+  squares.forEach((square) => square.addEventListener("dragend", dragEnd));
+  squares.forEach((square) => square.addEventListener("dragover", dragOver));
+  squares.forEach((square) => square.addEventListener("dragenter", dragEnter));
+  squares.forEach((square) => square.addEventListener("drageleave", dragLeave));
+  squares.forEach((square) => square.addEventListener("drop", dragDrop));
+
+  function dragStart() {
+    colorBeingDragged = this.style.backgroundImage;
+    squareIdBeingDragged = parseInt(this.id);
+  }
+
+  function dragOver(e) {
+    e.preventDefault();
+  }
+
+  function dragEnter(e) {
+    e.preventDefault();
+  }
+
+  function dragLeave() {
+    this.style.backgroundImage = "";
+  }
+
+  function dragDrop() {
+    colorBeingReplaced = this.style.backgroundImage;
+    squareIdBeingReplaced = parseInt(this.id);
+    this.style.backgroundImage = colorBeingDragged;
+    squares[squareIdBeingDragged].style.backgroundImage = colorBeingReplaced;
+  }
+
+  function dragEnd() {
+    // valid moves (only 1 swap place be it up, right, down or left)
+    let validMoves = [
+      squareIdBeingDragged - 1,
+      squareIdBeingDragged - width,
+      squareIdBeingDragged + 1,
+      squareIdBeingDragged + width,
+    ];
+    let validMove = validMoves.includes(squareIdBeingReplaced);
+
+    // make sure the color swap takes effect if the move is valid
+    if (squareIdBeingReplaced && validMove) {
+      squareIdBeingReplaced = null;
+    } else if (squareIdBeingReplaced && !validMove) {
+      squares[squareIdBeingReplaced].style.backgroundImage = colorBeingReplaced;
+      squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged;
+    } else
+      squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged;
+  }
 });
