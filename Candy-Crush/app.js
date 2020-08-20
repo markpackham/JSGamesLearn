af3c445859924fdb91e3bf87a78cfebf2acf9421
mark <markpackham1@gmail.com>
2020-08-20 12:15:13 +0100

    start initial setup for candy crush https://youtu.be/XD5sZWxwJUk?t=279

---
+++ Candy-Crush/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const scoreDisplay = document.getElementById("score");
+  const width = 8;
+  const squares = [];
+  let score = 0;
+
+  const candyColors = [
+    "url(images/red-candy.png)",
+    "url(images/yellow-candy.png)",
+    "url(images/orange-candy.png)",
+    "url(images/purple-candy.png)",
+    "url(images/green-candy.png)",
+    "url(images/blue-candy.png)",
+  ];
+});
