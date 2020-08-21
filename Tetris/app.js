828b066d5d69b58ce0052cf67e6b897ecc6e8e51
mark <markpackham1@gmail.com>
2020-08-21 15:42:33 +0100

    add mini grid block preview

--- Tetris/app.js
+++ Tetris/app.js
@@ -150,8 +150,8 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition = 4;
       draw();
       displayShape();
-      addScore();
-      gameOver();
+      // addScore();
+      // gameOver();
     }
   }
 
@@ -165,5 +165,33 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     current = theTetrominoes[random][currentRotation];
     draw();
+  }
+
+  // Mini Grid
+  // mini grid to provide future Tetrominos falling
+  const displaySquares = document.querySelectorAll(".mini-grid div");
+  const displayWidth = 4;
+  const displayIndex = 0;
+
+  // Tetrominos displayed in mini grid
+  const upNextTetrominoes = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2], //lTetromino
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], //zTetromino
+    [1, displayWidth, displayWidth + 1, displayWidth + 2], //tTetromino
+    [0, 1, displayWidth, displayWidth + 1], //oTetromino
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1], //iTetromino
+  ];
+
+  function displayShape() {
+    // remove any trace of a Tetromino form the entire grid
+    displaySquares.forEach((square) => {
+      square.classList.remove("tetromino");
+      square.style.backgroundColor = "";
+    });
+    upNextTetrominoes[nextRandom].forEach((index) => {
+      displaySquares[displayIndex + index].classList.add("tetromino");
+      displaySquares[displayIndex + index].style.backgroundColor =
+        colors[nextRandom];
+    });
   }
 });
