0b54b8b1f5eef042fc74ac0740c67bdb225de53a
mark <markpackham1@gmail.com>
2020-08-21 14:46:42 +0100

    add freeze and fall down functions

--- Tetris/app.js
+++ Tetris/app.js
@@ -75,4 +75,34 @@ document.addEventListener("DOMContentLoaded", () => {
       squares[currentPosition + index].style.backgroundColor = "";
     });
   }
+
+  // move blocks down
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
+    freeze();
+  }
+
+  // freeze
+  function freeze() {
+    if (
+      current.some((index) =>
+        squares[currentPosition + index + width].classList.contains("taken")
+      )
+    ) {
+      current.forEach((index) =>
+        squares[currentPosition + index].classList.add("taken")
+      );
+      // start a new tetromino falling
+      random = nextRandom;
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+      displayShape();
+      addScore();
+      gameOver();
+    }
+  }
 });
