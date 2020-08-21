09f2ed8a161851d2911d4f55986dba11b626d7cd
mark <markpackham1@gmail.com>
2020-08-21 15:44:46 +0100

    add start button and check for game over

--- Tetris/app.js
+++ Tetris/app.js
@@ -97,6 +97,7 @@ document.addEventListener("DOMContentLoaded", () => {
     currentPosition += width;
     draw();
     freeze();
+    gameOver();
   }
 
   // move left if no obsticals
@@ -194,4 +195,60 @@ document.addEventListener("DOMContentLoaded", () => {
         colors[nextRandom];
     });
   }
+
+  // Add to the score
+  function addScore() {
+    for (let i = 0; i < 199; i += width) {
+      const row = [
+        i,
+        i + 1,
+        i + 2,
+        i + 3,
+        i + 4,
+        i + 5,
+        i + 6,
+        i + 7,
+        i + 8,
+        i + 9,
+      ];
+
+      if (row.every((index) => squares[index].classList.contains("taken"))) {
+        score += 10;
+        scoreDisplay.innerHTML = score;
+        row.forEach((index) => {
+          squares[index].classList.remove("taken");
+          squares[index].classList.remove("tetromino");
+          squares[index].style.backgroundColor = "";
+        });
+        const squaresRemoved = squares.splice(i, width);
+        squares = squaresRemoved.concat(squares);
+        squares.forEach((cell) => grid.appendChild(cell));
+      }
+    }
+  }
+
+  // check if game over
+  function gameOver() {
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      scoreDisplay.innerHTML = "end";
+      clearInterval(timerId);
+    }
+  }
+
+  // Game start button
+  startBtn.addEventListener("click", () => {
+    if (timerId) {
+      clearInterval(timerId);
+      timerId = null;
+    } else {
+      draw();
+      timerId = setInterval(moveDown, 1000);
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+      displayShape();
+    }
+  });
 });
