79a78531394a18dffc7dcecbd65508eed1de6251
mark <markpackham1@gmail.com>
2020-08-21 16:22:53 +0100

    Initial structure for minesweeper learned from https://www.youtube.com/watch?v=rxdGAKRndz8

---
+++ Minesweeper/app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const flagsLeft = document.querySelector("#flags-left");
+  const result = document.querySelector("#result");
+  let width = 10;
+  let bombAmount = 20;
+  let flags = 0;
+  let squares = [];
+  let isGameOver = false;
+
+  // create board
+  function createBoard() {}
+});
