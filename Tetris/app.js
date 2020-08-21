d5ddab696e55930f24abc86594836bdb142ba882
mark <markpackham1@gmail.com>
2020-08-21 16:17:31 +0100

    prevent rotations for breaking through edge of screen

--- Tetris/app.js
+++ Tetris/app.js
@@ -157,6 +157,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  // Rotation
   // rotation
   function rotate() {
     undraw();
@@ -166,7 +167,34 @@ document.addEventListener("DOMContentLoaded", () => {
       currentRotation = 0;
     }
     current = theTetrominoes[random][currentRotation];
+    checkRotatedPosition();
     draw();
+  }
+
+  // Handle rotations at the edge of the screen
+  function isAtRight() {
+    return current.some((index) => (currentPosition + index + 1) % width === 0);
+  }
+
+  function isAtLeft() {
+    return current.some((index) => (currentPosition + index) % width === 0);
+  }
+
+  function checkRotatedPosition(P) {
+    P = P || currentPosition; // get current position then, check if the piece is near the left side.
+    if ((P + 1) % width < 4) {
+      // add 1 because the position index can be 1 less than where the piece is (with how they are indexed).
+      if (isAtRight()) {
+        // use actual position to check if it's flipped over to right side
+        currentPosition += 1; // if so, add one to wrap it back around
+        checkRotatedPosition(P); //check again then pass position from start, since long block might need to move more.
+      }
+    } else if (P % width > 5) {
+      if (isAtLeft()) {
+        currentPosition -= 1;
+        checkRotatedPosition(P);
+      }
+    }
   }
 
   // Mini Grid
