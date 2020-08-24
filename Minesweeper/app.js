edf6ce6be6c0f6f57966dcadb24a345bbbb7d611
mark <markpackham1@gmail.com>
2020-08-24 11:54:05 +0100

    click on square

--- Minesweeper/app.js
+++ Minesweeper/app.js
@@ -84,4 +84,31 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   createBoard();
+
+  // click on square
+  function click(square) {
+    let currentId = square.id;
+    if (isGameOver) return;
+    if (
+      square.classList.contains("checked") ||
+      square.classList.contains("flag")
+    )
+      return;
+    if (square.classList.contains("bomb")) {
+      gameOver(square);
+    } else {
+      let total = square.getAttribute("data");
+      if (total != 0) {
+        square.classList.add("checked");
+        if (total == 1) square.classList.add("one");
+        if (total == 2) square.classList.add("two");
+        if (total == 3) square.classList.add("three");
+        if (total == 4) square.classList.add("four");
+        square.innerHTML = total;
+        return;
+      }
+      checkSquare(square, currentId);
+    }
+    square.classList.add("checked");
+  }
 });
