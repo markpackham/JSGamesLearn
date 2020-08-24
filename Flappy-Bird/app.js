8cf9bd69cbf01a74f2c356dc779ec97593a596c4
mark <markpackham1@gmail.com>
2020-08-24 13:03:22 +0100

    add bird jump ability https://youtu.be/gxHcW84izz0?t=792

--- Flappy-Bird/app.js
+++ Flappy-Bird/app.js
@@ -5,7 +5,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let birdLeft = 220;
   let birdBottom = 100;
-  let gravity = 3;
+  let gravity = 2;
   let isGameOver = false;
   let gap = 430;
+
+  function startGame() {
+    birdBottom -= gravity;
+    bird.style.bottom = birdBottom + "px";
+    bird.style.left = birdLeft + "px";
+  }
+
+  let timerId = setInterval(startGame, 20);
+
+  function control() {}
+
+  function jump() {
+    // make sure bird can't jump out of screen
+    if (birdBottom < 500) birdBottom += 50;
+    bird.style.bottom = birdBottom + "px";
+  }
+  document.addEventListener("keyup", jump);
 });
